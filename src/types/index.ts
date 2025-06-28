94adaeb48836fa3e66127f0e01f639a9d6f8b444
benjaa-a11 <benjabostero223@gmail.com>
2025-06-28 01:03:35 -0300

    Add files via upload

--- src/types/index.ts
+++ src/types/index.ts
@@ -16,14 +16,15 @@ export type ChannelOption = {
 export type Match = {
   id: string;
   team1: string;
-  team1Logo: string;
+  team1Logo?: string;
   team2: string;
-  team2Logo: string;
+  team2Logo?: string;
   time: string;
   isLive: boolean;
+  isWatchable: boolean;
   channels: ChannelOption[];
   matchDetails?: string;
   matchTimestamp: Date;
-  tournamentName: string;
-  tournamentLogo: string | { light: string; dark: string };
+  tournamentName?: string;
+  tournamentLogo?: string | { light: string; dark: string };
 };
