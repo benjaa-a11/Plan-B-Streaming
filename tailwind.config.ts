1df4777f972a1085927895993795f5251409ff74
benjaa-a11 <benjabostero223@gmail.com>
2025-06-29 05:08:51 -0300

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,12 @@ export default {
       },
     },
     extend: {
+      spacing: {
+        'safe-top': 'env(safe-area-inset-top)',
+        'safe-bottom': 'env(safe-area-inset-bottom)',
+        'safe-left': 'env(safe-area-inset-left)',
+        'safe-right': 'env(safe-area-inset-right)',
+      },
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
         fifa: ["FIFASans", ...fontFamily.sans],
