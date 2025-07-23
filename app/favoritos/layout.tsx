18495047e6aed73929f6d7d2488141ddba625f7c
benjaa-a11 <benjabostero223@gmail.com>
2025-07-23 17:15:36 -0300

    Delete app directory

--- app/favoritos/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function FavoritesLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <>{children}</>;
-}
--- app/peliculas/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function MoviesLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div className="w-full bg-background">{children}</div>;
-}
