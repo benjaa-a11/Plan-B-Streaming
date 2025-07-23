c59167f671b4239307e1e4a01533a770efa1f46e
benjaa-a11 <benjabostero223@gmail.com>
2025-07-23 17:15:12 -0300

    Delete components directory

--- components/icons.tsx
+++
@@ -1,5 +0,0 @@
-// This file can be used to export custom SVG icons as React components.
-// Example:
-// export const MyIcon = (props: React.SVGProps<SVGSVGElement>) => ( <svg {...props}>...</svg> );
-
-export {};
